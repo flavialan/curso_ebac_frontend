f0261df6756183384371fcae56177a3dc325653a
Flavia Lopes <lalmeida.flavia@gmail.com>
2024-01-25 16:26:01 -0500

    entrega de exercicio

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaDoiNumeros(a, b) {
+    return a * b;
+}
+console.log(multiplicaDoiNumeros(2, 3));
+function dizOla(nome) {
+    return `Ola, ` + nome;
+}
+console.log(dizOla('Flavia'));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicaDoiNumeros (a : number, b : number) : number{
+    return a * b;
+}
+
+console.log(multiplicaDoiNumeros(2, 3));
+
+function dizOla (nome : string) : string {
+    return `Ola, ` + nome;
+}
+
+console.log(dizOla('Flavia'));
